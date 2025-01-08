2d659657ffd854ba8cad3df73dcd59e4c89e63a3
yashksaini-coder <ys3853428@gmail.com>
2025-01-08 23:25:10 +0530

    fix: 🐛 Add global prisma client

--- lib/database/prismaClient.ts
+++ lib/database/prismaClient.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  const globalAny: any = global;
+  if (!globalAny.prisma) {
+    globalAny.prisma = new PrismaClient();
+  }
+  prisma = globalAny.prisma;
+}
 
 export default prisma;
