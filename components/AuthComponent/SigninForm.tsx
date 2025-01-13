b8dd1101f712106e708867990a5bc9777d1a3dbb
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2025-01-13 14:32:33 +0530

    sigin done

--- components/AuthComponent/SigninForm.tsx
+++ components/AuthComponent/SigninForm.tsx
@@ -51,83 +51,98 @@ export default function SigninForm() {
 
   return (
     <main className="flex min-h-screen items-center justify-center px-4">
-      <Card className="w-full max-w-sm rounded-2xl">
-        <CardHeader>
-          <CardTitle>Sign in</CardTitle>
-          <CardDescription>
-            Enter your credentials to access your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="you@example.com" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+      <div className="grid md:grid-cols-2 grid-cols-1 md:dark:bg-dark-auth md:bg-light-auth md:bg-no-repeat object-cover rounded-xl md:p-10 gap-10">
+        <Card className="w-full md:border-none md:bg-white/10 backdrop-blur-3xl max-w-sm rounded-2xl">
+          <CardHeader>
+            <CardTitle>Sign in</CardTitle>
+            <CardDescription>
+              Enter your credentials to access your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="you@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="••••••••"
+                            {...field}
+                          />
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            size="sm"
+                            className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                            onClick={() => setShowPassword(!showPassword)}
+                          >
+                            {showPassword ? (
+                              <EyeOff className="h-4 w-4" />
+                            ) : (
+                              <Eye className="h-4 w-4" />
+                            )}
+                          </Button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {signinError && (
+                  <Alert variant="destructive">
+                    <AlertDescription>{signinError}</AlertDescription>
+                  </Alert>
                 )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div className="relative">
-                        <Input
-                          type={showPassword ? "text" : "password"}
-                          placeholder="••••••••"
-                          {...field}
-                        />
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="sm"
-                          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {showPassword ? (
-                            <EyeOff className="h-4 w-4" />
-                          ) : (
-                            <Eye className="h-4 w-4" />
-                          )}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {signinError && (
-                <Alert variant="destructive">
-                  <AlertDescription>{signinError}</AlertDescription>
-                </Alert>
-              )}
-              <LoadingButton
-                loading={isSigningIn}
-                loadingTitle="Signing in"
-                title="Sign in"
-                type="submit"
-              />
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex justify-center">
-          <AuthBottom
-            href="/auth/register"
-            title="Don't have an account? "
-            toTitle="Create One!"
-          />
-        </CardFooter>
-      </Card>
+                <LoadingButton
+                  loading={isSigningIn}
+                  loadingTitle="Signing in"
+                  title="Sign in"
+                  type="submit"
+                />
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <AuthBottom
+              href="/auth/register"
+              title="Don't have an account? "
+              toTitle="Create One!"
+            />
+          </CardFooter>
+        </Card>
+        <div className="h-96 md:flex hidden flex-col justify-center w-96">
+          <div>
+            <h1 className="text-7xl font-semibold">Welcome</h1>
+            <h1 className="text-7xl font-semibold">Back</h1>
+          </div>
+
+          <h2 className="text-2xl mt-4">
+            We're glad to see you again! Please sign in to continue.
+          </h2>
+        </div>
+      </div>
     </main>
   );
 }
