63a44777fe4ee657071979ee0e9d87f2d9ea87e2
yashksaini-coder <ys3853428@gmail.com>
2025-01-16 00:46:28 +0530

    refactor: 🗑️ Remove unused exports for fetchSingleProblem and fetchProblems controllers

--- app/api/leetcode/controllers/index.ts
+++ app/api/leetcode/controllers/index.ts
@@ -1,4 +1,2 @@
-export { default as fetchSingleProblem } from './fetchSingleProblem';
-export { default as fetchProblems } from './fetchProblems';
 export { default as fetchUserDetails } from './fetchUserDetails';
 export { default as fetchDataRawFormat } from './fetchDataRawFormat';
