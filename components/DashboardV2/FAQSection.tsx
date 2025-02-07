3754ef1f0c222966e3337c50a52011b6ba1fefbb
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2025-02-07 21:58:14 +0530

    Added Content

--- components/DashboardV2/FAQSection.tsx
+++ components/DashboardV2/FAQSection.tsx
@@ -53,7 +53,7 @@ const FAQList: FAQProps[] = [
 
 export const FAQSection = () => {
   return (
-    <section id="faq" className="container mx-auto md:w-[700px] py-24 sm:py-32">
+    <section id="faq" className="container mx-auto md:w-[900px] py-24 sm:py-32">
       <div className="text-center mb-8">
         <h2 className="text-lg text-indigo-500 text-center mb-2 tracking-wider">
           FAQS
@@ -67,11 +67,11 @@ export const FAQSection = () => {
       <Accordion type="single" collapsible className="AccordionRoot">
         {FAQList.map(({ question, answer, value }) => (
           <AccordionItem key={value} value={value}>
-            <AccordionTrigger className="text-left">
+            <AccordionTrigger className="text-left text-lg">
               {question}
             </AccordionTrigger>
 
-            <AccordionContent>{answer}</AccordionContent>
+            <AccordionContent className="text-lg">{answer}</AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
