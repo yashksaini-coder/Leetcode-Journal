531e0cb97c9fcec286feccb29f5466b16bdea6ec
yashksaini-coder <ys3853428@gmail.com>
2025-02-08 12:30:56 +0530

    refactor: Remove LandingEmerlandButton and LoadingButton components; update ToggleTheme labels for clarity

--- components/DashboardV2/ToggleTheme.tsx
+++ components/DashboardV2/ToggleTheme.tsx
@@ -13,15 +13,13 @@ export const ToggleTheme = () => {
     >
       <div className="flex gap-2 dark:hidden">
         <Moon className="size-5" />
-        <span className="block lg:hidden"> Escuro </span>
+        <span className="block lg:hidden"> Dark </span>
       </div>
 
       <div className="dark:flex gap-2 hidden">
         <Sun className="size-5" />
-        <span className="block lg:hidden">Claro</span>
+        <span className="block lg:hidden">Light</span>
       </div>
-
-      <span className="sr-only">Trocar de tema</span>
     </Button>
   );
 };
