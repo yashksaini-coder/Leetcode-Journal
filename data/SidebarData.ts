893e2242304ba2905953215d392b2d0f49b3314f
yashksaini-coder <ys3853428@gmail.com>
2025-03-02 01:07:14 +0530

    feat: add Dashboard link to SidebarData

--- data/SidebarData.ts
+++ data/SidebarData.ts
@@ -2,6 +2,11 @@ import { BookA, FileQuestion, Settings } from "lucide-react";
 
 export const SidebarData = [
     {
+      title: "Dashboard",
+      icon: Settings,
+      href: "/dashboard",
+    },
+    {
       title: "Problems",
       icon: FileQuestion,
       href: "/dashboard/problems",
