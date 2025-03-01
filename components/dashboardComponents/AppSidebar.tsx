cfb934cefa5ed465bb0e0f4440688a5fc8c0d299
yashksaini-coder <ys3853428@gmail.com>
2025-03-02 01:19:36 +0530

    refactor: uncomment theme toggle components in AppSidebar and DashboardNavbar

--- components/dashboardComponents/AppSidebar.tsx
+++ components/dashboardComponents/AppSidebar.tsx
@@ -91,7 +91,7 @@ export function AppSidebar() {
             isCollapsed ? "flex-col space-y-2" : "justify-between"
           )}
         >
-          {/* <Button
+          <Button
             className="rounded-full"
             size="icon"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
@@ -99,7 +99,7 @@ export function AppSidebar() {
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
-          </Button> */}
+          </Button>
           <Logout/>
         </div>
       </div>
